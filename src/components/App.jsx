3309f3dd47a0e827de6f8c29498caa93c2af8cb1
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-05-24 09:55:49 +0200

    feat: add main list of columns and tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,34 @@
 import '../styles/App.scss'
 import Header from './Header'
+import data from '../services/data.json'
 
 function App() {
+
+  const listColumns = data.boards[0].columns.map((column, index) => {
+    return (
+      <section key={index}>
+        <h2>{column.name}</h2>
+        <span>{column.tasks.length}</span>
+        <ul>
+          <li>
+            <article>
+              <h3>
+                {column.tasks.map(task => {
+                  return task.title
+                })}
+              </h3>
+            </article>
+          </li>
+        </ul>
+      </section>)
+  })
+
   return (
     <div>
       <Header></Header>
+      <main>
+        {listColumns}
+      </main>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import logo from '../images/logo-mobile.svg'
-import data from '../data.json'
+import data from '../services/data.json'
 
 const Header = () => {
     return (
@@ -7,7 +7,9 @@ const Header = () => {
             <div>
             <img src={logo} alt='logo task master'/>
             <h1>{data.boards[0].name}</h1>
-            <i className='fa fa-chevron-down'></i>
+            <button>
+                <i className='fa fa-chevron-down'></i>
+            </button>
             </div>
             <div>
             <button>
