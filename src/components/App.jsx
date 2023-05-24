7dea0f6c6f5eedef1d88197600e220d2e5bf8ebb
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-05-24 08:59:27 +0200

    feat: add Header component

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import '../styles/App.scss'
+import Header from './Header'
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import logo from '../images/logo-mobile.svg'
+import data from '../data.json'
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+            <img src={logo} alt='logo task master'/>
+            <h1>{data.boards[0].name}</h1>
+            <i className='fa fa-chevron-down'></i>
+            </div>
+            <div>
+            <button>
+                <i className='fa-solid fa-plus'></i>
+            </button>
+            <button>
+                <i className='fa-solid fa-ellipsis-vertical'></i>
+            </button>
+            </div>
+      </header>
+    )
+}
+
+export default Header
