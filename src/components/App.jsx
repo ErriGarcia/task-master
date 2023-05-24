a84e7e46c8d26d2ee88054519caed56837f2ef0c
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-05-24 10:19:30 +0200

    feat: add style to header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,7 @@ function App() {
   })
 
   return (
-    <div>
+    <div className='main'>
       <Header></Header>
       <main>
         {listColumns}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,26 @@
+import '../styles/Header.scss'
 import logo from '../images/logo-mobile.svg'
 import data from '../services/data.json'
 
 const Header = () => {
     return (
-        <header>
-            <div>
-            <img src={logo} alt='logo task master'/>
-            <h1>{data.boards[0].name}</h1>
-            <button>
-                <i className='fa fa-chevron-down'></i>
-            </button>
+        <header className='main-header'>
+            <div className='main-header-container-logo'>
+                <img src={logo} alt='logo task master'/>
+                <div className='main-header-container-logo-container'>
+                    <h1 className='main-header-container-logo-container-title'>{data.boards[0].name}</h1>
+                    <button className='main-header-container-logo-container-button-down'>
+                        <i className='fa fa-chevron-down'></i>
+                    </button>
+                </div>
             </div>
-            <div>
-            <button>
-                <i className='fa-solid fa-plus'></i>
-            </button>
-            <button>
-                <i className='fa-solid fa-ellipsis-vertical'></i>
-            </button>
+            <div className='main-header-container-buttons'>
+                <button className='main-header-container-buttons-button-plus'>
+                    <i className='fa-solid fa-plus'></i>
+                </button>
+                <button>
+                    <i className='fa-solid fa-ellipsis-vertical'></i>
+                </button>
             </div>
       </header>
     )
