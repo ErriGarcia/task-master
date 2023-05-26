b94498e25fcdf4bd99fdae8bdbad937f71323547
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-05-26 09:02:45 +0200

    feat: add icon to modalViewTask component

--- src/components/ModalViewTask.jsx
+++ src/components/ModalViewTask.jsx
@@ -18,6 +18,14 @@ const ModalViewTask = ({modal, setModal}) => {
         }
     }
 
+    const handleEvPreventDefault = (ev) => {
+        ev.preventDefault()
+    }
+
+    const handleClickMoreActions = () => {
+        //little modal
+    }
+
     const listOfSubtasks = firstColumn[0].tasks[0].subtasks.map((subtask, i) => {
         return ( <div key={i} className='container-view-task-subtasks-list-subtask' onChange={handleSubtaskForm}>
             <input type='checkbox' id={subtask.title} name={subtask.title} checked={subtask.isCompleted ? 'checked' : check}></input>
@@ -30,8 +38,13 @@ const ModalViewTask = ({modal, setModal}) => {
         <>
             {modal && (
                 <div className='modal' onClick={handleCloseModal}>
-                    <form className='container-view-task'>
-                        <h2 className='container-view-task-title'>{firstColumn[0].tasks[0].title}</h2>
+                    <form className='container-view-task' onClick={handleEvPreventDefault}>
+                        <fieldset className='container-view-task-header'>
+                            <h2 className='container-view-task-header-title'>{firstColumn[0].tasks[0].title}</h2>
+                            <button onClick={handleClickMoreActions}>
+                                <i className='fa-solid fa-ellipsis-vertical'></i>
+                            </button>
+                        </fieldset>
                         <p className='container-view-task-description'>
                             {/* {firstColumn[0].tasks[0].description} */}
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi alias assumenda blanditiis reprehenderit tempora cum magni nesciunt illo vero eum iure dolorum amet, eius repellendus pariatur quasi earum quia laudantium.
