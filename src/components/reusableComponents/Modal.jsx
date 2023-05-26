fb7a50471af57028158075a622af1d67214925c4
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-05-26 11:42:37 +0200

    feat: add style to modal

--- src/components/reusableComponents/Modal.jsx
+++ src/components/reusableComponents/Modal.jsx
@@ -7,30 +7,35 @@ const Modal = ({title, labelTitle, placeholderTitle, labelDescription, placehold
         <div className='container-view-task'>
             <form className='modal-form'>
                 <h2 className='modal-form-title'>{title}</h2>
-                <fieldset className='modal-form-fieldset-title'>
+                <fieldset className='modal-form-fieldset-title fieldset'>
                     <label htmlFor='title' className='label'>{labelTitle}</label>
                     <input type='text' id='title' name='title' className='input' placeholder={placeholderTitle} />
                 </fieldset>
-                <fieldset>
+                <fieldset className='fieldset'>
                     <label htmlFor='description' className='label'>{labelDescription}</label>
-                    <input type='text' id='description' name='description' placeholder={placeholderDescription} />
+                    <textarea type='text' id='description' name='description' className='textarea' placeholder={placeholderDescription}>
+                    </textarea>
                 </fieldset>
 
                 {/* Make reusable whole subtask fieldset */}
-                <fieldset>
+                <fieldset className='fieldset'>
                     <label htmlFor='subtasks' className='label'>{labelSubtasks}</label>
-                    <input type='text' id='subtasks' name='subtasks' placeholder={placeholderSubtask} />
-                    <button>
-                        <span className='material-symbols-outlined'>
-                            close
-                        </span>
-                    </button>
-                    <input type='text' id='subtasks' name='subtasks' placeholder={placeholderSubtask} />
-                    <button>
-                        <span className='material-symbols-outlined'>
-                            close
-                        </span>
-                    </button>
+                    <div className='container-subtasks'>
+                        <input type='text' id='subtasks' name='subtasks' className='input subtask' placeholder={placeholderSubtask} />
+                        <button className='button-delete'>
+                            <span className='material-symbols-outlined'>
+                                close
+                            </span>
+                        </button>
+                    </div>
+                    <div className='container-subtasks'>
+                        <input type='text' id='subtasks' name='subtasks' className='input subtask' placeholder={placeholderSubtask} />
+                        <button className='button-delete'>
+                            <span className='material-symbols-outlined'>
+                                close
+                            </span>
+                        </button>
+                    </div>
 
                     {/* Make reusable second button */}
                     <button>
